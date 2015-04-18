6f9ef624bcf206aa157ed9e84e3eb05a3685950d
Lovro Predovan <lovro.predovan@gmail.com>
2015-04-18 03:53:09 +0200

    c'moooon

--- blog/plugins/fb_comments.php
+++ blog/plugins/fb_comments.php
@@ -3,8 +3,8 @@ class fb_comments {
         public function after_load_content(&$file, &$content) {
                 $url = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
                 include 'fb_api.php';
-                $fb = "<br><div class='fb-comments' data-href='".$url."' data-num-posts='2' data-width='40%'></div>";
-                $content = $fb_sdk.$content;
+                $fb = "<br><div class='fb-comments' data-href='".$url."' data-num-posts='2' data-width='470px'></div>";
+                $content = $fb_sdk.$content.$social_share.$fb;
         }
 }
 ?>
